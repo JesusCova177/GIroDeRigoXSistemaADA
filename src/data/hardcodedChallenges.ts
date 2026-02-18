6a46111262463ec3a589344b7653bc43b103339b
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-18 02:49:07 -0400

    Updated hardcodedChallenges.ts

--- src/data/hardcodedChallenges.ts
+++ src/data/hardcodedChallenges.ts
@@ -69,7 +69,7 @@ export const hardcodedStage1Challenges: Challenge[] = [
   {
     id: 'hardcoded-4',
     stage_id: 'c893f30f-8b81-4393-a958-4f1dd9d383e4',
-    type: 'preamble_checklist',
+    type: 'practical_example',
     title: 'Distribución de Macronutrientes',
     content: {
       title: 'Distribución de Macronutrientes',
