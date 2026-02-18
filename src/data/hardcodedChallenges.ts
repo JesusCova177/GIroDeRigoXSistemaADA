573508bb628cbfa86d1c2783f50364c1fed55ea8
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-18 02:06:34 -0400

    Updated hardcodedChallenges.ts

--- src/data/hardcodedChallenges.ts
+++ src/data/hardcodedChallenges.ts
@@ -5,7 +5,6 @@ export const hardcodedStage1Challenges: Challenge[] = [
     id: 'hardcoded-1',
     stage_id: 'c893f30f-8b81-4393-a958-4f1dd9d383e4',
     type: 'intro',
-  
     content: {
       title: 'bajar grasa sin quedarte vacío',
       paragraphs: [
