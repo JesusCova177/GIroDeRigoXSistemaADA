576d64da13ade48837bc6a93543310d5da81d31b
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-18 03:25:22 -0400

    Updated hardcodedChallenges.ts

--- src/data/hardcodedChallenges.ts
+++ src/data/hardcodedChallenges.ts
@@ -78,7 +78,7 @@ export const hardcodedStage1Challenges: Challenge[] = [
     created_at: new Date().toISOString()
   },
    {
-    id: 'hardcoded-',
+    id: 'hardcoded-7',
     stage_id: 'c893f30f-8b81-4393-a958-4f1dd9d383e4',
     type: 'nutrition_guide',
     content: {
