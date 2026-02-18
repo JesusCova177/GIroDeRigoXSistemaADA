a085bdd7037cbb0faf13816e78b21865a8604f1b
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-18 02:29:49 -0400

    Updated hardcodedChallenges.ts

--- src/data/hardcodedChallenges.ts
+++ src/data/hardcodedChallenges.ts
@@ -9,7 +9,6 @@ export const hardcodedStage1Challenges: Challenge[] = [
       title: 'bajar grasa sin quedarte vacío',
       paragraphs: [
         'Comer menos para pesar menos: la fórmula “perfecta”. Y sí… al principio funciona, la balanza baja, uno se emociona y hasta se siente “juicioso”. El problema es que esa película dura poquito. La realidad me pegó de frente cuando le metí intensidad al entrenamiento y él cuerpo empezó a hablarme claro, pero yo no le pararle bolas.',
-        
       ]
     },
     order_index: 0,
