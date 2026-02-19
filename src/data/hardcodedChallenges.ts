eb96e7d30555e33c84a7e16bccdf44bc0848425c
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-19 12:04:16 -0400

    Updated hardcodedChallenges.ts

--- src/data/hardcodedChallenges.ts
+++ src/data/hardcodedChallenges.ts
@@ -8,7 +8,7 @@ export const hardcodedStage1Challenges: Challenge[] = [
     content: {
       title: 'bajar grasa sin quedarte vacío',
       paragraphs: [
-        'Comer menos para pesar menos: la fórmula “perfecta”.','Y sí… al principio funciona, la balanza baja, uno se emociona y hasta se siente “juicioso”.','El problema es que esa película dura poquito.','La realidad me pegó de frente cuando le metí intensidad al entrenamiento y él cuerpo empezó a hablarme claro, pero yo no le pararle bolas.',
+        'Comer menos para pesar menos: la fórmula “perfecta”.','Y sí… al principio funciona, la balanza baja, uno se emociona y hasta se siente “juicioso”.','El problema es que esa película dura poquito.','La realidad me pegó de frente cuando le metí intensidad al entrenamiento y él cuerpo empezó a hablarme claro, pero yo no le pararle bolas.', 'Montaba muchas horas, siempre a tope, y no le estaba dando a mi cuerpo las calorías ni los nutrientes que necesitaba.'
       ]
     },
     order_index: 0,
@@ -21,7 +21,7 @@ export const hardcodedStage1Challenges: Challenge[] = [
     content: {
       title: 'Y la embarré...',
       paragraphs: [
-        'Montaba muchas horas, siempre a tope, y no le estaba dando a mi cuerpo las calorías ni los nutrientes que necesitaba. Me descompensé y terminé con una desorientación tan grave que me tocó irme para urgencias. Ahí entendí algo que me cambió el rumbo: esto no se trata de castigar el cuerpo, se trata de estructurarlo. Si yo quería rendir en la bici y además perder grasa, tenía que aprender a comer con intención, no con culpa. '
+        'Me descompensé y terminé con una desorientación tan grave que me tocó irme para urgencias. Ahí entendí algo que me cambió el rumbo: esto no se trata de castigar el cuerpo, se trata de estructurarlo. Si yo quería rendir en la bici y además perder grasa, tenía que aprender a comer con intención, no con culpa. '
       ]
     },
     order_index: 0,
