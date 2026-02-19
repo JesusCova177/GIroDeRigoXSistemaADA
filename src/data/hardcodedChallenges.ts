c0864aff886640f01a1e8c5375a8a315cdcf4ba0
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-19 13:09:10 -0400

    Updated hardcodedChallenges.ts

--- src/data/hardcodedChallenges.ts
+++ src/data/hardcodedChallenges.ts
@@ -47,7 +47,7 @@ export const hardcodedStage1Challenges: Challenge[] = [
     type: 'intro',
     content: {
       title: 'Mi objetivo',
-      paragraphs: [ 'Yo aquí no estoy buscando que vos “adelgacés a las malas”','Yo estoy buscando que vos llegués más liviano, sí… pero también más fuerte, más estable y con energía real para entrenar.','Y dejame dejarte esto claro desde ya, porque es la trampa más común: yo no quiero que vos llegués en déficit a la carrera.', 'La semana del evento no es para “recortar” ni para “compensar aguantando hambre”', 'Eso es gasolina directa para la pálida.', 'La carrera se llega alimentado, con depósitos llenos y la cabeza tranquila.',' Listo. Ya viste el error. Ahora armemos la forma correcta de hacerlo.'
+      paragraphs: [ 'Yo aquí no estoy buscando que vos “adelgacés a las malas”','Yo estoy buscando que vos llegués más liviano, sí… pero también más fuerte, más estable y con energía real para entrenar.','Y dejame dejarte esto claro desde ya, porque es la trampa más común: yo no quiero que vos llegués en déficit a la carrera.', 'La semana del evento no es para “recortar” ni para “compensar aguantando hambre”', 'Eso es gasolina directa para la pálida. La carrera se llega alimentado, con depósitos llenos y la cabeza tranquila.',' Listo. Ya viste el error. Ahora armemos la forma correcta de hacerlo.'
       ]
     },
     order_index: 1,
