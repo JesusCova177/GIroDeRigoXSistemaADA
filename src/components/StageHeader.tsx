c7496910f70b0bcd44ca9242f5468af11881e837
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-20 18:19:13 -0400

    Updated StageHeader.tsx

--- src/components/StageHeader.tsx
+++ src/components/StageHeader.tsx
@@ -39,7 +39,7 @@ export function StageHeader({
         <div className="w-full flex  sm:flex-row justify-around items-center sm:items-center gap-2 sm:gap-3">
           {userEmail && onLogout && (
             <div className="flex items-center gap-2 bg-white/20 backdrop-blur-sm px-3 sm:px-4 py-2 rounded-xl shadow">
-              <div className="w-2 h-2 bg-green-500 rounded-full animate-pulse flex-shrink-0" />
+       
               
              
             </div>
