72b75b8863c0eca55f422a9427d61631558126f5
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-20 18:41:43 -0400

    Updated StageHeader.tsx

--- src/components/StageHeader.tsx
+++ src/components/StageHeader.tsx
@@ -41,7 +41,7 @@ export function StageHeader({
             <div >
               <button
                 onClick={onLogout}
-                className="ml-2 p-1 hover:bg-white/30 rounded transition-all duration-200"
+                className=""
               >
                 
               </button>
