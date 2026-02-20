ce25061e3d89de807825162aff424c1b9e53a221
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-20 17:12:31 -0400

    Updated IntroCard.tsx

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -19,7 +19,7 @@ export function IntroCard({ content }: IntroCardProps) {
   const { title, paragraphs, list, footer, callout } = content;
 
   return (
-    <div className="flexflex-col items-center rounded-2xl bg-white  border-2 border-[#f8a3c9] p-4 sm:p-6 md:p-8 mb-6 sm:mb-8 shadow-xl w-full">
+    <div className="flexflex-col items-center rounded-2xl bg-white border-2 border-[#f8a3c9] p-4 sm:p-6 md:p-8 mb-6 sm:mb-8 shadow-xl w-full h-[60dvh]">
       <div className="flex items-center justify-center gap-4 mb-4 sm:mb-6">
         <div>
           <h2 className="tracking-wide text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-titling font-black text-[#31563C] text-center mb-2 italic uppercase">
