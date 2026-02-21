bfb725959899cccb43b55b0eb99fa8a4972234c4
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-21 02:15:42 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {fontFamily: {
-        // 'custom' será el nombre de la clase: font-custom
-        'custom': ['Montserrat-Light', 'sans-serif'],},
+   
   },
   plugins: [],
 };
