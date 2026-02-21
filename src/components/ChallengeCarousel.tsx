96185a819725786056b4b516bc65a7aaa33c61f7
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-21 17:01:08 -0400

    Updated ChallengeCarousel.tsx

--- src/components/ChallengeCarousel.tsx
+++ src/components/ChallengeCarousel.tsx
@@ -294,7 +294,7 @@ export function ChallengeCarousel({
 
         <div
           ref={containerRef}
-          className="relative overflow-hidden cursor-grab active:cursor-grabbing"
+          className="relative cursor-grab active:cursor-grabbing"
           onTouchStart={handleTouchStart}
           onTouchMove={handleTouchMove}
           onTouchEnd={handleTouchEnd}
