a95df9e28309c02a719f878e1f63bc40067bb7ef
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-21 00:24:05 -0400

    Updated StageHeader.tsx

--- src/components/StageHeader.tsx
+++ src/components/StageHeader.tsx
@@ -37,18 +37,7 @@ export function StageHeader({
         }}
       >
         <div className="w-full">
-          {userEmail && onLogout && (
-            <div>
-       <span className="text-xs sm:text-sm font-medium text-[#31563C] truncate">
-              </span>
-              <button
-                onClick={onLogout}
-                className="ml-2 p-1 hover:bg-white/30 rounded transition-all duration-200"
-              >
-                <LogOut className="w-4 h-4 text-[#31563C]" />
-              </button>
-            </div>
-          )}
+        
          
         </div>
 
