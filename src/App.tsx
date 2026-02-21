fed44eb182ed70f9248d72cf58bad2cb62f4e8b9
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-20 20:35:01 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -208,9 +208,9 @@ function App() {
 
   return (
     <div className="h-vh bg-[#fff] overflow-hidden flex flex-col">
-       <span className="text-xl sm:text-sm font-medium text-black/90 block self-center justify-self-center text-center">
+       <h1 className="text-xl sm:text-sm font-medium text-black/90 block self-center justify-self-center text-center">
             Roadmap de Alimentación Giro de Rigo 2026<br/>By SISTEMA<span className="text-[#ffcc00]">ADA</span>
-            </span>
+            </h1>
       <div className="flex-1 min-h-0 overflow-y-auto">
         <div className="h-dvh max-w-4xl mx-auto px-3 sm:px-4 md:px-6 py-3 sm:py-4 overflow-hidden">
           <StageHeader
