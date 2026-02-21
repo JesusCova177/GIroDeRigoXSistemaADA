c84bcf2d031ace3581343bbb1a449562f995ca92
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-20 22:41:39 -0400

    Updated IntroCard.tsx

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -30,7 +30,7 @@ export function IntroCard({ content }: IntroCardProps) {
 
       <div className="space-y-4">
         {paragraphs.map((paragraph, index) => (
-          <p key={index} className=" leading-relaxed text-base sm:text-lg whitespace-pre-line font-[Montserrat-Ligth]">
+          <p key={index} className=" leading-relaxed text-base sm:text-lg whitespace-pre-line font-[Montserrat-Light]">
             {paragraph}
           </p>
         ))}
