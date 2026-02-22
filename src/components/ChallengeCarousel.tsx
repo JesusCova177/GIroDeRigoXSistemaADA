253e196bff5b041a6d40e6be6abfee0a38a14f0c
JesusCova177 <139079165+JesusCova177@users.noreply.github.com>
2026-02-21 21:44:06 -0400

    Updated ChallengeCarousel.tsx

--- src/components/ChallengeCarousel.tsx
+++ src/components/ChallengeCarousel.tsx
@@ -320,7 +320,7 @@ export function ChallengeCarousel({
               }
 
               return (
-                <div key={challenge.id} className=" flex-shrink-0 px-2 sm:px-4">
+                <div key={challenge.id} className="w-[1005] flex-shrink-0 px-2 sm:px-4">
                   {challenge.type === "intro" ? (
                     <IntroCard 
                       content={content} 
